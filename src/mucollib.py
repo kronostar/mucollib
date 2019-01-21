b1d3448922a87ef33fcf1b3aa362833de8589f4b
Steve Martin <kronostar.st4@gmail.com>
2019-01-21 20:55:10 +0000

    Start redesign of startPage() to incorporate buttons

--- src/mucollib.py
+++ src/mucollib.py
@@ -7,7 +7,7 @@
 #
 
 import tkinter as tk
-from tkinter import messagebox, Button, Listbox, Label, Scrollbar, Toplevel
+from tkinter import messagebox, Button, Listbox, Label, Scrollbar, Toplevel, Frame
 import sqlite3 as lite
 import sys
 
@@ -25,37 +25,40 @@ def msgImportData(db):
         dbimport.importCSV(db)
     
 def startPage():
-    btnFrame = tk.Frame(win,relief=tk.RAISED, borderwidth=1, bg = "black")
-    btnFrame.grid(column = 0, row = 1, columnspan = 27)
+    # frames for layout
+    topFrame = Frame(win,relief=tk.RAISED, borderwidth=1, bg = "black")
+    topFrame.grid(column = 0, row = 1, columnspan = 27)
+    listFrame = Frame(win)
+    listFrame.grid(column = 0, row = 2)
 
-    label1 = Label(win, text = "Artists", font = ("Helvetica", 16))
-    label1.grid(column = 10, row = 2)
-    scrollbar1 = Scrollbar(win, orient = tk.VERTICAL)
-    scrollbar1.grid(column = 11, row = 3, sticky = 'NS')
-    listbox1 = Listbox(win, width = 60, yscrollcommand = scrollbar1.set)
-    listbox1.grid(column = 10, row = 3)
+    label1 = Label(listFrame, text = "Artists", font = ("Helvetica", 16))
+    label1.grid(column = 0, row = 0)
+    scrollbar1 = Scrollbar(listFrame, orient = tk.VERTICAL)
+    scrollbar1.grid(column = 1, row = 1, sticky = 'NS')
+    listbox1 = Listbox(listFrame, width = 60, yscrollcommand = scrollbar1.set)
+    listbox1.grid(column = 0, row = 1)
     scrollbar1.config(command = listbox1.yview)
 
-    label2 = Label(win, text = "Albums", font = ("Helvetica", 16))
-    label2.grid(column = 12, row = 2)
-    scrollbar2 = Scrollbar(win, orient = tk.VERTICAL)
-    scrollbar2.grid(column = 13, row = 3, sticky = 'NS')
-    listbox2 = Listbox(win, width = 60, yscrollcommand = scrollbar2.set)
-    listbox2.grid(column = 12, row = 3)
+    label2 = Label(listFrame, text = "Albums", font = ("Helvetica", 16))
+    label2.grid(column = 2, row = 0)
+    scrollbar2 = Scrollbar(listFrame, orient = tk.VERTICAL)
+    scrollbar2.grid(column = 3, row = 1, sticky = 'NS')
+    listbox2 = Listbox(listFrame, width = 60, yscrollcommand = scrollbar2.set)
+    listbox2.grid(column = 2, row = 1)
     scrollbar2.config(command = listbox2.yview)
 
     listbox1.bind('<<ListboxSelect>>', lambda e : crud.selectArtist(e, listbox1, listbox2, db))
     listbox2.bind('<<ListboxSelect>>', lambda e : crud.selectAlbum(e, db))
 
     btext = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    label3 = Label(btnFrame, text = "Select Artist Group", bg = "black", fg = "white")
+    label3 = Label(topFrame, text = "Select Artist Group", bg = "black", fg = "white")
     label3.grid(column = 0, row = 0)
-    artistButton0 = Button(btnFrame, text="All")
+    artistButton0 = Button(topFrame, text="All")
     artistButton0.configure(command = lambda: crud.selectArtistGroup(db, 'All', listbox1, listbox2))
     artistButton0.grid(column = 1, row = 0)
     button = list()
     for i in range(26):
-        button.append(Button(btnFrame, text = btext[i]))
+        button.append(Button(topFrame, text = btext[i]))
         button[i].configure(command = lambda x = btext[i]: crud.selectArtistGroup(db, x, listbox1, listbox2))
         button[i].grid(column = i + 2, row = 0)
     crud.selectArtistGroup(db, 'All', listbox1, listbox2)
